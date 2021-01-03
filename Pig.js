6dbe27d1d18c1ea8cc1e9780c314bd908f7ac0b2
Kenneth Le <75586023+LeKenneth@users.noreply.github.com>
2021-01-03 15:40:07 -0500

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,17 @@
+class Pig extends BaseClass  {
+    constructor(x, y) {
+     
+     super(x, y, 50, 50);
+     this.image = loadImage("sprites/enemy.png");
+  }
+    
+};
+
+  //inheritance
+  //parent class/ base-class
+  //child class/ sub=class
+  //child class inherits properties and fucntions from parent class
+  //BaseClass
+  //child classes - pig, bird, box, log
+
+  
